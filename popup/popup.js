adeb03c899a4213639ed629e25946da701f8e5f7
Vineeth <18vvoruganti.stem@gmail.com>
2018-09-05 17:33:49 -0400

    fix adding Id's and added comments, add more depth to the State Object

--- popup/popup.js
+++ popup/popup.js
@@ -1,22 +1,36 @@
-const State = {
-    links: ["www.google.com","reddit.com"],
-    DOMElementId:'default',
-    addLinks: (array) => {
+const State = { //Initialization of the main state object that is going to be used to contain the links for each state
+    links: ["www.google.com","reddit.com"], //array that will hold the links for a particular state
+    DOMElementId:'default', //ID of the button that a State object will be linked to
+    addLink: (array) => { //function that can be used to add additional links to a state object parameters will have an array of links to add. 
         for(let i = 0; i < array.length ; i++) {
             array[i].push(this.links);
         }
+    },
+    removeLink:(array) => { //function that can be used to delete links from a state object
+        for(let i = 0; i < array.length; i++) {
+         
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() { //Equivalent of the Jquery on document ready, waits until page loads to run code 
-    const body = document.body;
+function State(links,DomElementId) { //constructor function for the State object
+    
+}
 
-    for(let i = 0; i <10; i++) {
-        body.appendChild(document.createElement('div')).className = "state-link";
+document.addEventListener("DOMContentLoaded", function() { //Equivalent of the Jquery on document ready, waits until page loads to run code 
+    const body = document.body; // set body of the DOM to a constant value. 
+
+    for(let i = 0; i <10; i++) { // for loop that adds first 10 links initially. 
+        let tempDiv = document.createElement('div'); // assigns a new Div element to a temporary variable
+        tempDiv.className="state-link" // gives the div element a generic class of state-link
+        tempDiv.id = "link-" + i; //gives the div a unique id based on number
+        body.appendChild(tempDiv); // adds the div to the body
+
+        
     }
 
 });
 
-function loadState() {
-
+function loadState() { //function for loading the browsing session based on a state
 }
+
+

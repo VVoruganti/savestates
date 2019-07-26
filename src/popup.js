29724c388b197de61a28fc96524ec128d5d4599a
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2019-07-25 22:11:11 -0400

    Add event listener to add new savestates from a UI perspective

--- src/popup.js
+++ src/popup.js
@@ -32,4 +32,38 @@
 // function loadState() { //function for loading the browsing session based on a state
 // }
 
+console.log("loaded")
 
+let stateTracker = 1;
+
+document.querySelector("#add-state").addEventListener("click", (event) => {
+    
+    console.log("click-event");
+    
+    stateTracker++;
+    
+    let container = document.createElement("div");
+    let title = document.createElement("div");
+    let edit = document.createElement("div");
+
+    let titleText = document.createElement("p");
+    let editText = document.createElement("p");
+
+    container.className = "stateButton";
+    container.id = `state-${stateTracker}`
+
+    title.className = "title";
+    edit.className = "edit-state";
+
+    titleText.innerHTML = "Lorem Ipsum";
+    editText.innerHTML = "edit";
+
+    title.append(titleText);
+    edit.append(editText);
+
+    container.append(title, edit);
+
+
+    document.querySelector("#states").append(container);
+
+});
